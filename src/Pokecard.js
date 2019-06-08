f9282e9505eb2811b2e7b708e34fb2d1afc6019e
Daniel <dan@dragomirweb.com>
2019-06-08 15:43:26 +0300

    pokecard image and data

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,13 +1,25 @@
-import React,{ Component } from 'react';
+import React, { Component } from "react";
 
 class Pokecard extends Component {
-    render() {
-        return (
-            <div className="Pokecard">
-                <p>Pokecard</p>
+  render() {
+    const id = this.props[0].id;
+    return (
+      <div className="Pokecard">
+        <div className="Pokecard--card">
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}></img>
         </div>
-    )
+      </div>
+    );
+  }
 }
-}
-
+Pokecard.defaultProps = [
+  { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+  { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+  { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+  { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+  { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+  { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+  { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+  { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+];
 export default Pokecard;
